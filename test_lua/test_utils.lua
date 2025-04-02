ead877d52811e5c74e3ab8bbd17f2b7313fb0de5
cepthomas <cepthomas@gmail.com>
2025-04-02 16:25:40 -0500

    WIP.

--- test_lua/test_utils.lua
+++ test_lua/test_utils.lua
@@ -1,5 +1,6 @@
 
 local sx = require("stringex")
+local tx = require("tableex")
 local ut = require("lbot_utils")
 
 -- print('!!!', package.path)
@@ -22,38 +23,38 @@ function M.suite_dump_table(pn)
     -- Test dump_table().
     local tt = { aa="pt1", bb=90901, alist={ "qwerty", 777, temb1={ jj="pt8", b=true, temb2={ num=1.517, dd="strdd" } }, intx=5432}}
 
-    local d = ut.dump_table(tt, 0)
+    local d = tx.dump_table(tt, 0)
     pn.UT_EQUAL(#d, 4)
 
-    d = ut.dump_table(tt, 1)
+    d = tx.dump_table(tt, 1)
     pn.UT_EQUAL(#d, 8)
 
-    d = ut.dump_table(tt, 2)
+    d = tx.dump_table(tt, 2)
     pn.UT_EQUAL(#d, 11)
 
-    d = ut.dump_table(tt, 3)
+    d = tx.dump_table(tt, 3)
     pn.UT_EQUAL(#d, 13)
 
-    d = ut.dump_table(tt, 4)
+    d = tx.dump_table(tt, 4)
     pn.UT_EQUAL(#d, 13)
 
-    local s = ut.dump_table_string(tt, 0, 'doody')
+    local s = tx.dump_table_string(tt, 0, 'doody')
     pn.UT_EQUAL(#s, 94)
 
-    s = ut.dump_table_string(tt, 1)
+    s = tx.dump_table_string(tt, 1)
     pn.UT_EQUAL(#s, 191)
 
-    s = ut.dump_table_string(tt, 2)
+    s = tx.dump_table_string(tt, 2)
     pn.UT_EQUAL(#s, 272)
 
-    s = ut.dump_table_string(tt, 3)
+    s = tx.dump_table_string(tt, 3)
     pn.UT_EQUAL(#s, 316)
 
-    s = ut.dump_table_string(tt, 4)
+    s = tx.dump_table_string(tt, 4)
     pn.UT_EQUAL(#s, 316)
 
     local tl = {'aaa', 'bbb', 'ccc', 'ddd', 'eee'}
-    s = ut.dump_list(tl)
+    s = tx.dump_list(tl)
     print(s)
     pn.UT_EQUAL(#s, 19)
 
