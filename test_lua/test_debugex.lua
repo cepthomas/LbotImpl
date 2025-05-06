bb8e62e4190d1faa9fc9cc8601848c71f7d4a655
cepthomas <cepthomas@gmail.com>
2025-05-06 17:20:48 -0500

    WIP.

--- test_lua/test_debugex.lua
+++ test_lua/test_debugex.lua
@@ -1,26 +1,20 @@
 
-local ut = require("lbot_utils")
-local lt = require("lbot_types")
-local sx = require("stringex")
-local tx = require('tableex')
+-- local ut = require("lbot_utils")
+-- local lt = require("lbot_types")
+-- local sx = require("stringex")
+-- local tx = require('tableex')
 
 
 local dbg = require("debugex")
 
 local counter = 100
 
+
+
 dbg.print('Loading test_debugex.lua')
 
 
 -----------------------------------------------------------------------------
--- fake script here:
-
-
------------------------------------------------------------------------------
---- Global function for App interaction with script internals.
--- @param cmd specific command string
--- @param arg optional argument int
--- @return result string (table would be nice later)
 local function do_command(cmd, arg)
     dbg.print('Got this command: '..cmd..'('..arg..')')
     local ret = 'counter => '..counter
@@ -39,6 +33,32 @@ end
 
 -----------------------------------------------------------------------------
 local function nest1(some_arg)
+    local my_table =
+    {
+        aa="str-pt1",
+        mt={},
+        bb=90901,
+        alist=
+        {
+            "str-qwerty",
+            777.888,
+            temb1=
+            {
+                jj="str-pt8",
+                b=true,
+                temb2=
+                {
+                    num=1.517,
+                    dd="string-dd"
+                }
+            },
+            intx=5432,
+            nada=nil
+        },
+        cc=function() end,
+        [101]='booga'
+    }
+
     dbg.print('nest1() was called: '..some_arg)
     dbg()
     nest2(some_arg..'1')
@@ -53,11 +73,13 @@ end
 
 --------------- Start here --------------------------------------------------
 
-print('do_command():', do_command('touch', 'nose'))
+-- Plain function.
+local ok, msg = do_command('touch', 'nose')
+dbg.print(string.format('do_command(): %q %s', ok, msg))
 
--- print('boom(orange):', boom('orange'))
-
-print('boom(green):', dbg.pcall(boom, 'green')) -- TODOD so func/line doess not get update after this is called.
+-- Function that error().
+ok, msg = dbg.pcall(boom, 'green')
+dbg.print(string.format('boom(green): %q %s', ok, msg))
 
 -- local ok, msg = dbg.pcall(boom, 'green')
 -- if not ok then
